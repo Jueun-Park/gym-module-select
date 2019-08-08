f986cb8766985816faf4ccb485207ccbac8b3338
Jueun-Park <elpion19@gmail.com>
2019-08-08 17:17:15 +0900

    update module_select_env.py

--- gym_module_select/envs/module_select_env.py
+++ gym_module_select/envs/module_select_env.py
@@ -1,5 +1,5 @@
 import gym
-from gym import error, spaces, utils
+from gym import error, spaces
 from gym.utils import seeding
 import numpy as np
 import time
@@ -30,18 +30,19 @@ class ModuleSelectEnv(gym.Env):
     continuous = False
 
     def __init__(self):
+        self.verbose = 1
+
         stats_path = "logs/sac/DonkeyVae-v0-level-0_6/DonkeyVae-v0-level-0"
         hyperparams, stats_path = get_saved_hyperparams(
             stats_path, norm_reward=False)
         hyperparams['vae_path'] = "logs/vae-level-0-dim-32.pkl"
         self.inner_env = create_test_env(
             stats_path=stats_path, seed=0, log_dir="logs", hyperparams=hyperparams)
-        self.inner_obs = self.inner_env.reset()
 
         self.detector = LaneDetector()
         # TODO: wanna get original image of first frame
-        self.raw_obs = np.ones((80, 160, 3), dtype=np.uint8)
-        self.detector.detect_lane(self.raw_obs)
+        # self.raw_obs = np.ones((80, 160, 3), dtype=np.uint8)
+        # self.raw_obs, _, _, _ = self.inner_env.envs[0].env.viewer.observe()
 
         algo = "sac"
         model_path = "logs/sac/DonkeyVae-v0-level-0_6/DonkeyVae-v0-level-0.pkl"
@@ -54,11 +55,7 @@ class ModuleSelectEnv(gym.Env):
             # lane detection, end-to-end
             self.action_space = spaces.Discrete(2)
         
-        self.observation_space = spaces.Box(low=0, high=255, shape=self.raw_obs.shape)
-
-        self.processing_times = []
-        self.num_default = 0
-        self.num_vae_sac = 0
+        self.observation_space = spaces.Box(low=0, high=255, shape=(80, 160, 3))
 
     def step(self, action):
         if self.continuous:
@@ -69,6 +66,7 @@ class ModuleSelectEnv(gym.Env):
         if action == 0:
             # default line tracer
             self.num_default += 1
+            self.raw_obs, _, _, _ = self.inner_env.envs[0].env.viewer.observe()
             _, angle_error = self.detector.detect_lane(self.raw_obs)
             angle_error = -angle_error
             steer = steer_controller(angle_error)
@@ -76,38 +74,48 @@ class ModuleSelectEnv(gym.Env):
             speed = base_speed - np.abs(reduction)
             inner_action = [[steer, speed]]
 
+            check_processing_time(start_time, self.processing_times)
+
             self.inner_obs, reward, done, infos = self.inner_env.step(
                 inner_action)
         elif action == 1:
             # VAE-SAC agent
             self.num_vae_sac += 1
             inner_action, _ = self.model.predict(
-                self.inner_obs, deterministic=False)
+                self.inner_obs, deterministic=True)
             # Clip Action to avoid out of bound errors
             if isinstance(self.inner_env.action_space, gym.spaces.Box):
-                inner_action = np.clip(action, self.inner_env.action_space.low,
+                inner_action = np.clip(inner_action, self.inner_env.action_space.low,
                                        self.inner_env.action_space.high)
+            print(inner_action)
+            check_processing_time(start_time, self.processing_times)
+            
             self.inner_obs, reward, done, infos = self.inner_env.step(
-                [inner_action])
+                inner_action)
         else:
             print("action error")
 
-        check_processing_time(start_time, self.processing_times)
-
-        self.raw_obs = infos[0]['raw_obs']
         cv2.imshow('input', self.detector.original_image_array)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             pass
 
         # TODO: ?
-        reward -= self.processing_times[-1]
+        reward[0] -= self.processing_times[-1]
+        self.running_reward += reward[0]
+        self.ep_len += 1
 
-        return self.raw_obs, reward, done, infos[0]
+        return self.raw_obs, reward[0], done, infos[0]
 
     def reset(self):
         self.inner_obs = self.inner_env.reset()
-        self.raw_obs = np.ones((80, 160, 3), dtype=np.uint8)  # TODO
-        self._print_counting_log()
+        # self.raw_obs = np.ones((80, 160, 3), dtype=np.uint8)  # TODO
+        self.raw_obs, _, _, _ = self.inner_env.envs[0].env.viewer.observe()
+        self.detector.detect_lane(self.raw_obs)
+        if self.verbose == 1:
+            self._print_counting_log()
+        self.running_reward = 0
+        self.ep_len = 0
+        self.processing_times = []
         self.num_default = 0
         self.num_vae_sac = 0
 
@@ -124,11 +132,15 @@ class ModuleSelectEnv(gym.Env):
 
     def _print_counting_log(self):
         try:
+            print("Episode Reward: {:.2f}".format(self.running_reward))
+            print("Episode Length", self.ep_len)
             print("Default:", self.num_default, "/ VAE-SAC:", self.num_vae_sac,
                 "/ Default ratio: {:.2f}".format(self.num_default / (self.num_default+self.num_vae_sac)))
             print("One frame processing time mean (ms): ",
                 1000 * np.mean(self.processing_times))
         except ZeroDivisionError:
+            pass
+        except AttributeError:
             pass
 
 

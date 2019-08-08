7bc5776df1b77a1dc2b4ec4079cdd1e20c58b2c3
Jueun-Park <elpion19@gmail.com>
2019-08-08 17:15:40 +0900

    avoid import error

--- gym_module_select/envs/__init__.py
+++ gym_module_select/envs/__init__.py
@@ -1,2 +1,5 @@
 from gym_module_select.envs.vae_env import *
-from gym_module_select.envs.module_select_env import *
+try:
+    from gym_module_select.envs.module_select_env import *
+except ImportError:
+    pass

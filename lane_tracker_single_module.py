f2abf38aeb3446cdc52307920085b474862bcb6b
Jueun-Park <elpion19@gmail.com>
2019-08-20 22:07:19 +0900

    single modules

---
+++ lane_tracker_single_module.py
@@ -0,0 +1,17 @@
+import gym
+import sys
+sys.path.append('.')
+import gym_module_select
+from stable_baselines.common.vec_env import DummyVecEnv
+
+
+env = gym.make('ModuleSelect-v0')
+env = DummyVecEnv([lambda: env])
+
+obs = env.reset()
+for i in range(1000):
+    action = [0]
+    obs, rewards, dones, info = env.step(action)
+    env.render()
+
+env.close()

b256ae111625fe29a4fce28067abe2ccbabeee07
Jueun-Park <elpion19@gmail.com>
2019-09-08 22:44:14 +0900

    clip action

--- modules/vae_sac_modules.py
+++ modules/vae_sac_modules.py
@@ -7,12 +7,15 @@ from modules.algos import SAC
 
 class VAESACModule:
     def __init__(self, sac_vae_env: DonkeyVAEEnv, model: SAC, delay_weight):
+        self.env = sac_vae_env
         self.model = model
         self.delay_weight = delay_weight
 
     def predict(self, obs):
-        self._add_delay_from_distribution()
+        self._add_delay_from_distribution(obs[-1])  # TODO:
         action, _ = self.model.predict(obs, deterministic=True)
+        if isinstance(self.env.action_space, gym.spaces.Box):
+            action = np.clip(action, self.env.action_space.low, self.env.action_space.high)
         return action
 
     def _add_delay_from_distribution(self, proc_state):

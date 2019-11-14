b9313baf366c73cf2ae1a829522edbc8f55bee34
Jueun-Park <elpion19@gmail.com>
2019-11-14 23:43:07 +0900

    train dqn

---
+++ train/train_dqn.py
@@ -0,0 +1,84 @@
+import argparse
+import os
+import numpy
+import gym
+import sys
+sys.path.append('.')
+import time
+import gym_module_select
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines.deepq.policies import MlpPolicy
+from stable_baselines.bench import Monitor
+from stable_baselines.results_plotter import load_results, ts2xy
+from stable_baselines import DQN
+
+TIMESTEPS = int(1e8) + 1
+
+
+def init_parse_argument():
+    parser = argparse.ArgumentParser()
+    timestr = time.strftime("%H%M%S")
+    parser.add_argument('-i', '--id', help='nickname of the train', type=str, default=timestr)
+    args = parser.parse_args()
+    return args
+
+
+best_mean_reward = -numpy.inf
+n_steps = 0
+args = init_parse_argument()
+datestr = time.strftime("%Y%m%d")
+log_directory = os.path.dirname(os.path.realpath(__file__)) + "/dn-dqn-log-" + args.id + "-" +  datestr + "/"
+model_directory = os.path.dirname(os.path.realpath(__file__)) + "/dn-dqn-models-" + args.id + "-" + datestr + "/"
+
+
+def callback(_locals, _globals):
+    """
+    Callback called at each step (for DQN an others) or after n steps (see ACER or PPO2)
+    :param _locals: (dict)
+    :param _globals: (dict)
+    """
+    global best_mean_reward, n_steps
+    if (n_steps + 1) % 10000 == 0:
+        best_mean_reward = 0  # for saving model by force
+        # controls per step = 10 : save each 500 steps
+        # 1 : 1000 (maybe..?..)
+    if (n_steps + 1) % 500 == 0:
+        x, y = ts2xy(load_results(log_directory), 'timesteps')
+        if len(x) > 0:
+            mean_reward = numpy.mean(y[-100:])
+            print(x[-1], 'timesteps')
+            print("Best mean reward: {:.2f} - Last mean reward per episode: {:.2f}".format(
+                best_mean_reward, mean_reward))
+
+        if mean_reward > best_mean_reward:
+            best_mean_reward = mean_reward
+            print("Saving new best model")
+            _locals['self'].save(
+                model_directory + 'dqn-model_' + str(n_steps + 1) + '.pkl')
+    n_steps += 1
+    return True
+
+
+if __name__ == "__main__":
+    os.makedirs(log_directory, exist_ok=True)
+    os.makedirs(model_directory, exist_ok=True)
+
+    env = gym.make('ModuleSelect-v1',
+                    verbose=0,
+                    )
+    env = Monitor(env, log_directory, allow_early_resets=True)
+    env = DummyVecEnv([lambda: env])
+    model = DQN(
+        env=env,
+        policy=MlpPolicy,
+        verbose=1,
+        tensorboard_log="./daynight_dqn_tensorboard/",
+        batch_size=32,
+    )
+    try:
+        model.learn(
+            total_timesteps=TIMESTEPS,
+            callback=callback
+        )
+    except KeyboardInterrupt:
+        env.envs[0].env.close()

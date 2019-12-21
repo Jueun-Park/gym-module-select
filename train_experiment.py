a59c681027071b770f8ca1be30729f4497326ddc
Jueun-Park <elpion19@gmail.com>
2019-12-21 20:39:56 +0900

    slow period exp; git reset to do not use raw image

--- train_experiment.py
+++ train_experiment.py
@@ -1,7 +1,8 @@
 import os
 
-timesteps = 70001  # about: 10h 30m * 4 = 42h
+timesteps = 1000001  # about: 10h 30m * 4 = 42h
 # timesteps = 11
-lr_list = [1e-4, 2e-4, 3e-4, 4e-4]
-for lr in lr_list:
-    os.system("python train/train_dqn.py -l " + str(lr) + " -t " + str(timesteps))
+# lr_list = [1e-4, 2e-4, 3e-4, 4e-4]
+lr = 1e-4
+for i in range(3):
+    os.system("python train/train_dqn.py -l " + str(lr) + " -t " + str(timesteps) + " -i 1221slow")

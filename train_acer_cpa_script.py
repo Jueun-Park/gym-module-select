a5e3b6b18240c44813a6913625e07b8fd723cd82
Jueun-Park <elpion19@gmail.com>
2020-01-31 14:03:09 +0900

    stacked state expr

--- train_acer_cpa_script.py
+++ train_acer_cpa_script.py
@@ -2,11 +2,18 @@ from os import system
 
 
 # about 24h for one experiment, total 3 days (?!)
-cpa_and_timestep = [
-    [5, 400001],
-    [10, 200001],
-    [20, 100001],
-]
+# cpa_and_timestep = [
+#     [5, 400001],
+#     [10, 200001],
+#     [20, 100001],
+# ]
 
-for cpa, timestep in cpa_and_timestep:
-    system("python train/train_acer.py -i test_cpa --cpa " + str(cpa) + " -t " + str(timestep))
+# for cpa, timestep in cpa_and_timestep:
+#     system("python train/train_acer.py -i test_cpa --cpa " + str(cpa) + " -t " + str(timestep))
+
+cpa = 5
+timestep = 300001  # 300k, about 21h for one expr
+# timestep = 10
+learning_rates = [1e-4, 2e-4, 4e-4]
+for lr in learning_rates:
+    system("python train/train_acer.py -i lr_exp -l " + str(lr) + " --cpa " + str(cpa) + " -t " + str(timestep))
